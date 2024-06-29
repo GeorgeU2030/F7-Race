a57c94b8d9a43c8811a731ecfe6719e9df46baef
GeorgeU2030 <georgeu2021@gmail.com>
2024-06-28 19:59:55 -0500

    feat: components and routes has been added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: LandingComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'signup', component: SignupComponent},
+    {path: 'home', component: HomeComponent},
+    {path: '**', redirectTo: '', pathMatch: 'full'}
+];
