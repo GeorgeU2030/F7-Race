2cd141d0cab61512eb88fec45d04c69057d31459
GeorgeU2030 <georgeu2021@gmail.com>
2024-07-03 12:37:26 -0500

    feat: interceptor has been added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,15 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideToastr } from 'ngx-toastr';
 import { IMAGE_CONFIG } from '@angular/common';
+import { authInterceptor } from './custom/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), 
     provideAnimationsAsync(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideToastr(),
     {
       provide: IMAGE_CONFIG,
