e19862492bbfa36c197c7e9acf68cd67e4f90bd7
GeorgeU2030 <georgeu2021@gmail.com>
2024-07-03 13:02:19 -0500

    feat: guard has been added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { LandingComponent } from './pages/landing/landing.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { HomeComponent } from './pages/home/home.component';
+import { authGuard } from './custom/auth.guard';
 
 export const routes: Routes = [
     {path: '', component: LandingComponent},
     {path: 'login', component: LoginComponent},
     {path: 'signup', component: SignupComponent},
-    {path: 'home', component: HomeComponent},
+    {path: 'home', component: HomeComponent, canActivate: [authGuard]},
     {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
